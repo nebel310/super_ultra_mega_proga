c6f2d03be6c044fd2fd78cc463fd3b8503e10bec
Vlados <89387622+nebel310@users.noreply.github.com>
2023-12-20 14:18:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+import time
+
+
+
+
+aa = 'Получаю информацию...'
+bb = 'Произвожу вычисления...'
+cc = 'Анализирую результаты...'
+
+
+f = 'больше у...'
+z = []
+k = ['влада.', 'сани']
+def check_chlen():
+    global z
+    f = 'больше у...'
+    z = []
+    l = 0
+    v = 0
+    if l <= v or v > l:
+        l += 1
+    z.append(l)
+    z.append(v)
+    n = len(z)
+    for i in range(n, 0, -1):
+        for j in range(i):
+            try:
+                if z[j] > z[j+1]:
+                    z[j], z[j+1] = z[j+1], z[j]
+                    z[j] = 'vlad'
+                    z[j+1] = 'насрал'
+            except:
+                ...
+    return z
+
+
+pohuy = input("Введите имя пользователя 1 --->>> ")
+pohuy = input("Введите имя пользователя 2 --->>> ")
+
+user_1 = 'насрала'
+user_2 = 'влада'
+user_3 = 'loilded'
+
+
+
+check_chlen()
+time.sleep(1)
+print(aa)
+ss = '...'
+print(ss)
+time.sleep(1)
+print(bb)
+ss = '...'
+print(ss)
+time.sleep(1)
+print(cc)
+time.sleep(1)
+print(ss)
+
+time.sleep(1)
+print(f)
+time.sleep(0.5)
+print(ss)
+time.sleep(0.5)
+print(ss)
+time.sleep(2)
+print(ss)
+time.sleep(3)
+print(k[0])
